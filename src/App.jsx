9f3bbf0948cccc9e780fc67639407a92c14bc6cc
rrt2850 <79067594+rrt2850@users.noreply.github.com>
2025-01-18 21:31:51 -0500

    Initial commit ;)

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>
+          Wow this loads so quick
+        </h1>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
